5a4dd91cd9e71559ccec9d3ea6f35449eb374969
santosh0305 <2mymail.santosh@gmail.com>
2020-07-21 14:37:19 +0530

    1 file modified

--- src/app/controls/controls.module.ts
+++ src/app/controls/controls.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ControlsComponent } from "./controls.component";
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule],
+  declarations: [ControlsComponent]
 })
-export class ControlsModule { }
+export class ControlsModule {}
