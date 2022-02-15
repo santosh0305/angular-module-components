7c8de07a6f419eabadadc0625edd9c07d085dc21
santosh0305 <2mymail.santosh@gmail.com>
2022-02-15 12:56:58 +0530

    4 files modified, 2 deleted

--- src/app/controls/controls.module.ts
+++ src/app/controls/controls.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { ControlsComponent } from "./controls.component";
-import { ImageComponent } from "./image/image.component";
-import { TextboxComponent } from "./textbox/textbox.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlsComponent } from './controls.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ControlsComponent, ImageComponent, TextboxComponent],
-  exports: [ImageComponent]
+  declarations: [ControlsComponent, ImageComponent],
+  exports: [ImageComponent],
 })
 export class ControlsModule {}
